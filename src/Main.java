6c835e147b990e64b7d8719c65375b2b53b616cf
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-05-17 18:22:55 +0300

    1-st commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 43_800;
+        int bonus;
+        bonus = ticket / 20;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
